efa91f802b27191f5b6b22e1287069f045fd79f3
Hayimanot <haymimes@gmail.com>
2025-04-03 16:12:42 +0300

    Added images,search bar, and lists without styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
+      <h1>Recipe Finder</h1>
+      <form>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search for a recipe"
+          required
+        />
+      </form>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <article>
+          <h2>
+            Name of the recipe <span>Category</span>Region<span></span>
+          </h2>
+          <img
+            src="https://i.pinimg.com/736x/93/d5/d4/93d5d4b3d5932388b3e6ed1db050ac0f.jpg"
+            alt=""
+          />
+          <h3>Ingredients</h3>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Items 3</li>
+          </ul>
+          <h3>Instructions</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            officiis deserunt obcaecati voluptas ullam. Labore, voluptatibus
+            officiis nobis consectetur eius qui, natus dolore necessitatibus
+            aliquid quisquam non nesciunt fugiat fuga.
+          </p>
+          <ul>
+            <li>Video</li>
+            <li>Source</li>
+          </ul>
+        </article>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
