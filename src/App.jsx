65bf9f2b0bad653d555609289c91e285ae2b2d90
Hayimanot <haymimes@gmail.com>
2025-04-05 15:27:41 +0300

    Add API call to fetch random recipe in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
+  const [meals, setMeals] = useState([]);
+  const [meal, setMeal] = useState("");
+
+  async function getMeals() {
+    try {
+      const res = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+      );
+      const data = await res.json();
+      setMeals(data.meals);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getMeals();
+  }, []);
+
   return (
     <div className="py-20 px-2 max-w-4xl mx-auto">
       <h1 className="text-4xl lg:text-5xl text-neutral-900 bg-[#FEA609] rounded-lg font-bold text-center">
@@ -16,47 +36,48 @@ export default function App() {
         />
       </form>
       <div>
-        <article>
-          <h2 className="flex items-center gap-4 text-3xl lg:text-4xl text-neutral-900  font-bold mb-12">
-            Name of the recipe{" "}
-            <span className="bg-[#FEA609] font-normal text-sm px-2 rounded-full">
-              Category
-            </span>
-            <span className="bg-[#FEA609] font-normal text-sm px-2 rounded-full">
-              Region
-            </span>
-          </h2>
-          <img
-            className="w-full object-cover"
-            src="https://i.pinimg.com/736x/93/d5/d4/93d5d4b3d5932388b3e6ed1db050ac0f.jpg"
-            alt=""
-          />
-          <h3 className="text-2xl  text-neutral-900  font-bold my-8">
-            Ingredients
-          </h3>
-          <ul className="list-decimal pl-4">
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Items 3</li>
-          </ul>
-          <h3 className="text-2xl  text-neutral-900  font-bold my-8">
-            Instructions
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-            officiis deserunt obcaecati voluptas ullam. Labore, voluptatibus
-            officiis nobis consectetur eius qui, natus dolore necessitatibus
-            aliquid quisquam non nesciunt fugiat fuga.
-          </p>
-          <ul className="flex items-center justify-start flex-wrap mt-8">
-            <li className="bg-[#FEA609] font-normal text-base px-2 rounded-full">
-              Video
-            </li>
-            <li className="bg-[#FEA609] font-normal text-base px-2 rounded-full">
-              Source
-            </li>
-          </ul>
-        </article>
+        {meals.map((meal) => (
+          <article>
+            <h2 className="flex items-center gap-4 text-3xl lg:text-4xl text-neutral-900  font-bold mb-12">
+              {meal.strMeal}
+              <span className="bg-[#FEA609] font-normal text-sm px-2 rounded-full">
+                {meal.strCategory}
+              </span>
+              <span className="bg-[#FEA609] font-normal text-sm px-2 rounded-full">
+                {meal.strArea}
+              </span>
+            </h2>
+            <img
+              className="w-full"
+              src={meal.strMealThumb}
+              alt={meal.strMeal}
+            />
+            <h3 className="text-2xl  text-neutral-900  font-bold my-8">
+              Ingredients
+            </h3>
+            <ul className="list-decimal pl-4">
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Items 3</li>
+            </ul>
+            <h3 className="text-2xl  text-neutral-900  font-bold my-8">
+              Instructions
+            </h3>
+            <p>{meal.strInstructions}</p>
+            <ul className="flex items-center justify-start flex-wrap mt-8">
+              <li className="bg-[#FEA609] font-normal text-base px-2 rounded-full">
+                <a href={meal.strYoutube} target="_blank" rel="noreferrer">
+                  Video
+                </a>
+              </li>
+              <li className="bg-[#FEA609] font-normal text-base px-2 rounded-full">
+                <a href={meal.strSource} target="_blank" rel="noreferrer">
+                  Source
+                </a>
+              </li>
+            </ul>
+          </article>
+        ))}
       </div>
     </div>
   );
